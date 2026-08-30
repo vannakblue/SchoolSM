655b0d8e42cad3f3ccb2123bd655be38697a64c2
blue <vannakblue@gmail.com>
2026-08-30 20:37:57 +0700

    Patch compileSdkVersion and add afterEvaluate in subprojects for GitHub Actions Android build

--- schoolsm_mobile/android/build.gradle.kts
+++ schoolsm_mobile/android/build.gradle.kts
@@ -24,19 +24,21 @@ subprojects {
 }
 
 subprojects {
-    project.pluginManager.withPlugin("com.android.library") {
-        project.extensions.configure<com.android.build.gradle.LibraryExtension> {
-            if (namespace == null) {
-                namespace = when (project.name) {
-                    "qr_code_scanner" -> "net.touchcapture.qr.flutterqr"
-                    else -> "dev.flutter.plugins.${project.name.replace("-", "_")}"
+    afterEvaluate {
+        project.pluginManager.withPlugin("com.android.library") {
+            project.extensions.configure<com.android.build.gradle.LibraryExtension> {
+                if (namespace == null) {
+                    namespace = when (project.name) {
+                        "qr_code_scanner" -> "net.touchcapture.qr.flutterqr"
+                        else -> "dev.flutter.plugins.${project.name.replace("-", "_")}"
+                    }
                 }
-            }
-            compileSdk = 36
-            ndkVersion = "28.2.13676358"
-            compileOptions {
-                sourceCompatibility = JavaVersion.VERSION_17
-                targetCompatibility = JavaVersion.VERSION_17
+                compileSdk = 36
+                ndkVersion = "28.2.13676358"
+                compileOptions {
+                    sourceCompatibility = JavaVersion.VERSION_17
+                    targetCompatibility = JavaVersion.VERSION_17
+                }
             }
         }
     }
