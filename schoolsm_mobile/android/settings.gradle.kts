30501b07257afed5549a829d4de88d97ec6d816a
blue <vannakblue@gmail.com>
2026-08-30 13:59:51 +0700

    Auto-generate local.properties for CI Android APK build

--- schoolsm_mobile/android/settings.gradle.kts
+++ schoolsm_mobile/android/settings.gradle.kts
@@ -1,12 +1,16 @@
 pluginManagement {
-    val flutterSdkPath =
-        run {
-            val properties = java.util.Properties()
-            file("local.properties").inputStream().use { properties.load(it) }
-            val flutterSdkPath = properties.getProperty("flutter.sdk")
-            require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
-            flutterSdkPath
+    val flutterSdkPath = run {
+        val properties = java.util.Properties()
+        val localPropertiesFile = file("local.properties")
+        if (localPropertiesFile.exists()) {
+            localPropertiesFile.inputStream().use { properties.load(it) }
         }
+        val flutterSdkPath = properties.getProperty("flutter.sdk")
+            ?: System.getenv("FLUTTER_ROOT")
+            ?: System.getenv("FLUTTER_HOME")
+        require(flutterSdkPath != null) { "flutter.sdk not set in local.properties or environment variable" }
+        flutterSdkPath
+    }
 
     includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
 
